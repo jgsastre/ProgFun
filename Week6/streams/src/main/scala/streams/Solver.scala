a61bafa3a755808f819505f7c161e40612ba807a
XimoGarcia <jgsastre+github@gmail.com>
2014-06-29 17:13:08 +0200

    Added new implementation

--- Week6/streams/src/main/scala/streams/Solver.scala
+++ Week6/streams/src/main/scala/streams/Solver.scala
@@ -10,7 +10,7 @@ trait Solver extends GameDef {
   /**
    * Returns `true` if the block `b` is at the final position
    */
-  def done(b: Block): Boolean = ???
+  def done(b: Block): Boolean = goal == b
 
   /**
    * This function takes two arguments: the current block `b` and
@@ -28,7 +28,9 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = ???
+  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = {
+    (b.legalNeighbors.map {case (block, move) => (block, move :: history) }).toStream
+  }
 
   /**
    * This function returns the list of neighbors without the block
@@ -36,7 +38,13 @@ trait Solver extends GameDef {
    * make sure that we don't explore circular paths.
    */
   def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
-                       explored: Set[Block]): Stream[(Block, List[Move])] = ???
+                       explored: Set[Block]): Stream[(Block, List[Move])] = {
+    (for {
+      (block, history) <- neighbors
+      (new_block, new_history) <- neighborsWithHistory(block, history)
+      if (!(explored contains new_block))
+    } yield (new_block, new_history)).toStream
+  }
 
   /**
    * The function `from` returns the stream of all possible paths
@@ -62,7 +70,9 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] = for {
+             (block, history) <- newNeighborsOnly(initial, explored)
+           } yield 
 
   /**
    * The stream of all paths that begin at the starting block.
